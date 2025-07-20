2b8e6b81b32829828c07565b9d028a6b5cbb08ea
Guillaume Michel <GuillaumeMichel02@users.noreply.github.com>
2025-07-20 21:51:10 +0200

    fix build

--- entry.cpp
+++ entry.cpp
@@ -1,26 +1,32 @@
+#include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 #include "WFCChunk.hpp"
 
 using namespace godot;
 
-// This function registers the extension and the class
-extern "C" {
-GDExtensionBool GDExtensionInit(
-    GDExtensionInterface *p_interface,
-    GDExtensionClassLibraryPtr p_library,
-    GDExtensionInitialization *r_initialization) {
-
-    GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
-
-    // Register your class with Godot here
-    init_obj.register_initializer([]() {
-        ClassDB::register_class<WFCChunk>();
-    });
-
-    // Define when the extension should be initialized (scene = ready for nodes/resources)
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-    init_obj.initialize();
+void initialize_wfc_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
     
-    return true;
+    ClassDB::register_class<WFCChunk>();
+}
+
+void uninitialize_wfc_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
+
+extern "C" {
+// Initialization
+GDExtensionBool GDE_EXPORT wfc_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+    init_obj.register_initializer(initialize_wfc_module);
+    init_obj.register_terminator(uninitialize_wfc_module);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+    return init_obj.init();
 }
 }
